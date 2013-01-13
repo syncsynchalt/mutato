0063fcdf7fb172ae5997b58035f8f5ba805da217
Michael Driscoll <mdriscoll@pax8.com>
2013-01-12 19:25:51 -0700

    Shut up zip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e -x
 git clone git://github.com/syncsynchalt/mutato.git mutato-latest
 (cd mutato-latest && mvn package)
-zip -r mutato-latest.zip mutato-latest
+zip -qr mutato-latest.zip mutato-latest
 scp mutato-latest.zip w.ulfheim.net:/var/www/files/mutato/
 rm mutato-latest.zip
 rm -rf mutato-latest
